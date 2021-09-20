e63f80cc9aa0bffd3e0fc955aac8cedfdec4f8df
BrendanFinnegan <bf289907@gmail.com>
2021-09-20 17:17:20 -0400

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Change!
+          Test Change - dis one
         </p>
         <a
           className="App-link"
