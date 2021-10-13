e965aa9365e442c78645228a73e5245720346e18
BrendanFinnegan <bf289907@gmail.com>
2021-10-13 11:55:45 -0400

    errors done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :name, presence:true
+    validates :email, presence:true
+    validates :password, presence:true
     has_many :user_runes
     has_many :runes, through: :user_runes
 end
